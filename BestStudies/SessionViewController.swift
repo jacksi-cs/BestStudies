9b7a2cdaf25ceea2fa260e5d2535558957b58e62
Jack Si <jacksi@Jacks-MBP-4.attlocal.net>
2022-10-18 20:05:57 -0500

    Updated README

--- BestStudies/SessionViewController.swift
+++ BestStudies/SessionViewController.swift
@@ -10,48 +10,48 @@
 import UIKit
 
 class SessionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    
+
+
     @IBOutlet weak var totalTimeLabel: UILabel!
     @IBOutlet weak var membersTableView: UITableView!
-    
+
     var members:[String] = []
     var slackTimes:[TimeInterval] = []
     var leaveDates:[Date] = []
-    
+
     var isStopwatch:Bool?
     var totalTime:TimeInterval?
-    
+
     var generalTime:Timer = Timer()
-    
+
     var timeFormatter = DateComponentsFormatter()
-    
-    
-    
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         membersTableView.delegate = self
         membersTableView.dataSource = self
-        
+
         totalTime = (totalTime == nil) ? 0 : totalTime
-        
+
         members.append("Yourself")
         slackTimes.append(0.0)
         leaveDates.append(Date())
-        
+
         timeFormatter.unitsStyle = .abbreviated
         timeFormatter.zeroFormattingBehavior = .pad
         timeFormatter.allowedUnits = [.hour, .minute, .second]
-        
+
         NotificationCenter.default.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: nil) { [self](notification) in
             leaveDates[0] = Date()
         }
-        
+
         NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: nil) { [self](notification) in
             let difference = Calendar.current.dateComponents([.second], from: leaveDates[0], to: Date())
             countSlack(timePassed: Double(difference.second!), index: 0)
-            
+
             if self.isStopwatch! {
                 countUp(timePassed: Double(difference.second!))
             } else {
@@ -59,43 +59,43 @@ class SessionViewController: UIViewController, UITableViewDelegate, UITableViewD
             }
 
         }
-        
+
         if self.isStopwatch! {
             generalTime = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countUpOne), userInfo: nil, repeats: true)
         } else {
             generalTime = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDownOne), userInfo: nil, repeats: true)
         }
     }
-    
+
     @objc func countUpOne() -> Void {
         totalTime! += 1.0
         self.totalTimeLabel.text = timeFormatter.string(from: totalTime!)
     }
-    
+
     @objc func countDownOne() -> Void {
         totalTime! -= 1.0
         self.totalTimeLabel.text = timeFormatter.string(from: totalTime!)
     }
-    
+
     @objc func countUp(timePassed: Double) -> Void {
         totalTime! += timePassed
         self.totalTimeLabel.text = timeFormatter.string(from: totalTime!)
     }
-    
+
     @objc func countDown(timePassed: Double = 1.0) -> Void {
         totalTime! -= timePassed
         self.totalTimeLabel.text = timeFormatter.string(from: totalTime!)
     }
-    
+
     @objc func countSlack(timePassed: Double, index: Int) -> Void {
         slackTimes[index] += timePassed
         membersTableView.reloadData()
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         members.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = membersTableView.dequeueReusableCell(withIdentifier: "MemberCell", for: indexPath)
         let row = indexPath.row
@@ -103,7 +103,7 @@ class SessionViewController: UIViewController, UITableViewDelegate, UITableViewD
         cell.detailTextLabel?.text = timeFormatter.string(from: slackTimes[row])
         return cell
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // TODO: For the attempted implementation of mutliple swiping VCs
 //        if segue.identifier == "timeSegueIdentifier" {
@@ -111,7 +111,7 @@ class SessionViewController: UIViewController, UITableViewDelegate, UITableViewD
 //
 //        }
     }
-    
+
     @IBAction func leavePressed(_ sender: Any) {
         navigationController?.popViewController(animated: true)
         dismiss(animated: true)
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# BestStudies
-# BestStudies
+Contributions
+
+Jason:25%
+-Loading Screen
+-Login Screen
+-Registration Screen
+-README
+
+Joseph Yeh: 25%
+-Timer functionality
+-Timer screens
+
+Jack Greer: 25%
+-Settings Screens
+-Home Screen
+-Timer Screen
+
+Jack Si:25%
+-Merging project
+
+Deviations:
+-Got more done on login than we expected but still have more to do like creating Facebook and Google linking
+-Pushed Sound Detection down to stretch goals because even if added, would be an experimental/optional feature
+-For the sessions, our current session view is without swiping capabilities. We were having issues getting UIPageViewController working in container, and have pushed it to be resolved ASAP in Beta
+-Settings for Account, Linking, Sounds, and Notifications were not fully implemented as some of them required implementation of features that were slated for Beta or were not mentioned at all

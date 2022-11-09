06a62f2e2ec129af9918b3e9aedfb9347be61d0a
Jack Si <jacksi@Jacks-MBP-4.attlocal.net>
2022-11-08 19:51:52 -0600

    Underlying multipeer connection

---
+++ BestStudies/ConnectionManager.swift
@@ -0,0 +1,8 @@
+//
+//  ConnectionManager.swift
+//  BestStudies
+//
+//  Created by Jack Si on 11/8/22.
+//
+
+import Foundation
---
+++ BestStudies/WaitingRoomViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WaitingRoomViewController.swift
+//  BestStudies
+//
+//  Created by Jack Si on 11/8/22.
+//
+
+import UIKit
+
+class WaitingRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

3054e9f0513a88ad2de1d9959167c786848a361f
Jason Causey <jac23662@utexas.edu>
2022-11-14 20:18:52 -0600

    Stats and Achievement Page
    -Barebones stats and achievement page

--- BestStudies/AchievementsViewController.swift
+++ BestStudies/AchievementsViewController.swift
@@ -7,24 +7,34 @@
 
 import UIKit
 
-class AchievementsViewController: UIViewController {
+class AchievementsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let tableArray = ["Best Studier: \n Study more than anyone else in a group.",
+                        "Group Studier:  \n Study with 5 or more friends in a single session.",
+                        "Frequent Studier:  \n Study for longer than 1 hour cumulatively"]
+    
+    let cellIdentifier = "AchieveCellIdentifier"
+
+    @IBOutlet weak var achieveTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
 
         // Do any additional setup after loading the view.
+        achieveTableView.delegate = self
+        achieveTableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableArray.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = tableArray[row]
+        cell.backgroundColor = .white
+        return cell
+    }
 }
--- BestStudies/StatisticsViewController.swift
+++ BestStudies/StatisticsViewController.swift
@@ -7,24 +7,39 @@
 
 import UIKit
 
-class StatisticsViewController: UIViewController {
+class StatisticsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let tableArray = ["Total Study Time:  1h",
+                        "Total Sessions:  15",
+                        "Top Study Buddy:  Jack",
+                        "Top Study Time:  15m",
+                      "Worst Slacking Time  5m",
+                        "Worst Study Partner  Jim"]
+    
+    let cellIdentifier = "StatCellIdentifier"
+
+    @IBOutlet weak var statTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
 
         // Do any additional setup after loading the view.
+        statTableView.delegate = self
+        statTableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableArray.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = tableArray[row]
+        cell.backgroundColor = .white
+        return cell
+    }
+    
 
 }
